f99a78bf618c6c0d2f6a98a338188e273421b17d
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-08-24 11:55:51 +0100

    Update settings.py

--- main_app/settings.py
+++ main_app/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tfhoursng.herokuapp.com',]
 
 # Gmail smtp
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
