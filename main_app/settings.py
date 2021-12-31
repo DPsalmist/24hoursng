8adcfe91150a378c66d2b488e2823870cf3e45c6
Samuel Damilare <48826043+DPsalmist@users.noreply.github.com>
2021-12-31 04:04:21 +0100

    Update settings.py

--- main_app/settings.py
+++ main_app/settings.py
@@ -25,8 +25,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'damilaretesting@gmail.com'
+EMAIL_HOST_PASSWORD = 'damiistesting'
+#EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 #EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 
